6150a1c621e6b4f3f8d180bdc30b98ee60e5e697
Jack Danger Canty <jackdanger@squareup.com>
2016-07-13 18:18:47 -0700

    Created the runner.Executor
    It knows nothing about Transforms, it's one level lower than that.

---
+++ runner/executor.go
@@ -0,0 +1,175 @@
+package runner
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/JackDanger/traffic/model"
+)
+
+// Executor is anything that can perform HTTP requests (it's an interface so we
+// can mock it in tests)
+type Executor interface {
+	Get(model.Request) (*model.Response, error)
+	Post(model.Request) (*model.Response, error)
+	Put(model.Request) (*model.Response, error)
+	Delete(model.Request) (*model.Response, error)
+	Head(model.Request) (*model.Response, error)
+	Patch(model.Request) (*model.Response, error)
+}
+
+// HTTPExecutor has methods that accept a request from a HAR file and make a
+// connection to the actual URL specified in each request. The HTTP response is
+// then placed into a model.Response object and returned.
+type HTTPExecutor struct {
+	client      http.Client
+	logger      Logger
+	lastRequest *http.Request // only for testing
+}
+
+// Get performs an HTTP POST
+func (e *HTTPExecutor) Get(r model.Request) (*model.Response, error) {
+	req, err := http.NewRequest("GET", r.URL, nil)
+	fmt.Println(req.URL.Host)
+	if err != nil {
+		e.handleError(err)
+		return &model.Response{}, err
+	}
+
+	e.fromModelRequest(req, &r)
+
+	return e.toModelResponse(e.client.Do(req)), nil
+}
+
+// Post performs an HTTP POST
+func (e *HTTPExecutor) Post(r model.Request) (*model.Response, error) {
+	req, err := http.NewRequest("POST", r.URL, bytes.NewBufferString(r.PostData.Text))
+	if err != nil {
+		e.handleError(err)
+		return &model.Response{}, err
+	}
+
+	e.fromModelRequest(req, &r)
+
+	return e.toModelResponse(e.client.Do(req)), nil
+}
+
+// Put performs an HTTP PUT
+func (e *HTTPExecutor) Put(r model.Request) (*model.Response, error) {
+	return &model.Response{}, nil
+}
+
+// Delete performs an HTTP DELETE
+func (e *HTTPExecutor) Delete(r model.Request) (*model.Response, error) {
+	return &model.Response{}, nil
+}
+
+// Head is the equivalent of `curl -I`
+func (e *HTTPExecutor) Head(r model.Request) (*model.Response, error) {
+	return &model.Response{}, nil
+}
+
+// Patch is an HTTP verb we'll rarely see
+func (e *HTTPExecutor) Patch(r model.Request) (*model.Response, error) {
+	return &model.Response{}, nil
+}
+
+// NewHTTPExecutor returns an object that can perform live HTTP requests
+func NewHTTPExecutor(logDevice io.Writer) Executor {
+	return &HTTPExecutor{
+		client: http.Client{},
+		logger: NewLogger(logDevice),
+	}
+}
+
+func (e *HTTPExecutor) toModelResponse(h *http.Response, err error) *model.Response {
+	if err != nil {
+		e.handleError(err)
+		return &model.Response{}
+	}
+
+	headers := []model.SingleItemMap{}
+	for key, values := range h.Header {
+		// HTTP allows you to send the same header key multiple times and
+		// http.Header stores a collapsed version of all headers with the same key.
+		// Here we explode them back out.
+		for _, value := range values {
+			headers = append(headers, model.SingleItemMap{
+				// Make a copy of the string otherwise we store the address and this
+				// loop reuses the address each time through.
+				Key:   func(s string) *string { return &s }(key),
+				Value: &value,
+			})
+		}
+	}
+
+	body, err := ioutil.ReadAll(h.Body)
+	if err != nil {
+		e.logger.Println("error reading http response body: ", err)
+	}
+
+	return &model.Response{
+		HTTPVersion: h.Proto,
+		Status:      h.StatusCode,
+		StatusText:  h.Status,
+		Headers:     headers,
+		ContentBody: func(s string) *string { return &s }(string(body)),
+	}
+}
+
+func (e *HTTPExecutor) handleError(err error) {
+	e.log("error: ", fmt.Sprintf("%#v", err))
+}
+
+// Reads the headers from the model.Request and applies them to the
+// http.Request. Adds a Content-Type header if one is missing.
+func (e *HTTPExecutor) fromModelRequest(req *http.Request, modelRequest *model.Request) {
+	e.log(req.Method, ": ", req.URL)
+	e.lastRequest = req
+
+	contentTypeIsSet := false
+	for _, header := range modelRequest.Headers {
+		if *header.Key == "Content-Type" {
+			contentTypeIsSet = true
+		}
+		req.Header.Set(*header.Key, *header.Value)
+	}
+	if contentTypeIsSet {
+		return
+	}
+
+	// We must have manually provided a Content-Type header, don't try to intuit
+	// one from the PostData
+	if modelRequest.PostData != nil {
+		req.Header.Set("Content-Type", modelRequest.PostData.MimeType)
+	} else {
+		req.Header.Set("Content-Type", "text/html") // can't think of a better default
+	}
+}
+
+// Logger encapsulates printing to the screen or a file or a variable under test.
+type Logger struct {
+	device io.Writer
+}
+
+// Println sends to the logging device what fmt.Println sends to os.Stdout
+func (l *Logger) Println(s ...interface{}) {
+	l.device.Write([]byte(fmt.Sprintln(s...)))
+}
+
+func (e *HTTPExecutor) log(s ...interface{}) {
+	e.logger.Println(s...)
+}
+
+// GetLastRequest is used in testing to assert we've properly transformed inbound values
+func (e *HTTPExecutor) GetLastRequest() *http.Request {
+	return e.lastRequest
+}
+
+// NewLogger produces a logger backed by the provided device
+func NewLogger(device io.Writer) Logger {
+	return Logger{device: device}
+}
---
+++ runner/executor_test.go
@@ -0,0 +1,212 @@
+package runner
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/JackDanger/traffic/model"
+	util "github.com/JackDanger/traffic/test"
+)
+
+type handler struct{}
+
+var responseHeaders = map[string]string{}
+var responseBody = "default response body"
+var performedRequest *http.Request
+var performedRequestBody string
+
+// This boots a test server that listens on the local host and response to any
+// test client requests with headers and content stored in global variables.
+func (h *handler) ServeHTTP(w http.ResponseWriter, request *http.Request) {
+	// Store the incoming request so we can check it
+	performedRequest = request
+	// And read the request body so we can store that
+	body, _ := ioutil.ReadAll(request.Body)
+	performedRequestBody = string(body)
+
+	// set any headers defined in this test
+	for key, value := range responseHeaders {
+		w.Header().Set(key, value)
+	}
+	// set the body defined in this test
+	w.Write([]byte(responseBody))
+}
+
+// This starts a server and immediately backgrounds it via a goroutine
+func startServer() string {
+	server := http.Server{
+		Addr:    "127.0.0.1:9797",
+		Handler: &handler{},
+	}
+	go server.ListenAndServe()
+	// Wait a moment so the server can boot
+	time.Sleep(100 * time.Millisecond)
+	return "server is running"
+}
+
+var started = startServer()
+
+func TestGet(t *testing.T) {
+	println(started)
+	executor := NewHTTPExecutor(os.Stdout)
+
+	// Set some headers that our server will sent back to us - we'll check that
+	// they return in the right format
+	responseHeaders["Time"] = "noonish"
+	responseHeaders["Session"] = "valid-session"
+	// And define a body we hope to retrieve
+	responseBody = "This should be the body"
+
+	request := model.Request{
+		URL: "http://localhost:9797/",
+		Headers: []model.SingleItemMap{
+			{
+				Key:   util.StringPtr("X-REQUEST-HEADER"), // NB: net/http will modify the case of this string
+				Value: util.StringPtr("Val"),
+			},
+		},
+	}
+	response, err := executor.Get(request)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !hasHeader(stdLibHeadersToModel(performedRequest.Header), "X-Request-Header", "Val") {
+		t.Errorf("Expected our headers to make it to the server, got: \n%s", printSingleItemMaps(stdLibHeadersToModel(performedRequest.Header)))
+	}
+	if !hasHeader(response.Headers, "Time", "noonish") {
+		t.Errorf("Couldn't find the first test header in\n %s", printSingleItemMaps(response.Headers))
+	}
+	if !hasHeader(response.Headers, "Session", "valid-session") {
+		t.Errorf("Couldn't find the first second header in\n %s", printSingleItemMaps(response.Headers))
+	}
+	if *response.ContentBody != "This should be the body" {
+		t.Errorf("Unexpected body content: %s", response.ContentBody)
+	}
+}
+
+func TestGetWithElaborateHeadersAndQueryString(t *testing.T) {
+	println(started)
+	executor := NewHTTPExecutor(os.Stdout).(*HTTPExecutor)
+
+	request := model.Request{
+		URL: "http://localhost:9797/kate/heddleston?query=string",
+		QueryString: []model.SingleItemMap{
+			{
+				Key:   util.StringPtr("Second"),
+				Value: util.StringPtr("QueryValue"),
+			},
+		},
+		Headers: []model.SingleItemMap{
+			{
+				Key:   util.StringPtr("Official-Title"),
+				Value: util.StringPtr("Software Warrior Princess/CEO"),
+			},
+			{
+				Key:   util.StringPtr("Content-Type"),
+				Value: util.StringPtr("application/x-set-in-header-manually"),
+			},
+		},
+	}
+	_, err := executor.Get(request)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req := executor.GetLastRequest()
+
+	if !hasHeader(stdLibHeadersToModel(req.Header), "Official-Title", "Software Warrior Princess/CEO") {
+		t.Errorf("Expected our headers to make it to the server, got: \n%s", printSingleItemMaps(stdLibHeadersToModel(req.Header)))
+	}
+	if !hasHeader(stdLibHeadersToModel(req.Header), "Content-Type", "application/x-set-in-header-manually") {
+		t.Errorf("Expected our headers to make it to the server, got: \n%s", printSingleItemMaps(stdLibHeadersToModel(req.Header)))
+	}
+	if req.URL.Host != "localhost:9797" {
+		t.Errorf("host was not properly incorporated into the URL: %s, %s", req.URL.Host, req.URL)
+	}
+	if req.URL.Path != "/kate/heddleston" {
+		t.Errorf("path was not properly incorporated into the URL: %s, %s", req.URL.Path, req.URL)
+	}
+	if req.URL.RawQuery != "query=string" {
+		t.Errorf("query string was not properly incorporated into the URL: %s, %s", req.URL.RawQuery, req.URL)
+	}
+}
+
+func TestPost(t *testing.T) {
+	println(started)
+	executor := NewHTTPExecutor(os.Stdout)
+
+	// Set some headers that our server will sent back to us - we'll check that
+	// they return in the right format
+	responseHeaders["Time"] = "afternoon"
+	responseHeaders["Content-Type"] = "application/x-protobuf"
+	// And define a body we hope to retrieve
+	responseBody = "This should be the body"
+
+	response, err := executor.Post(model.Request{
+		URL: "http://localhost:9797/",
+		PostData: &model.PostData{
+			MimeType: "application/json",
+			Text:     `{"Nickname": "Jenny", "Role": "Team Captain"}`,
+		},
+	})
+
+	if response.Status != 200 {
+		t.Errorf("Expected HTTP 200 OK, got: %#v", response.Status)
+	}
+
+	if performedRequestBody != `{"Nickname": "Jenny", "Role": "Team Captain"}` {
+		t.Errorf("Expected a particular request body, got: %s ", performedRequestBody)
+	}
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !hasHeader(response.Headers, "Time", "afternoon") {
+		t.Errorf("Couldn't find the first test header in\n %s", printSingleItemMaps(response.Headers))
+	}
+
+	if !hasHeader(response.Headers, "Content-Type", "application/x-protobuf") {
+		t.Errorf("Couldn't find the second test header in\n %s", printSingleItemMaps(response.Headers))
+	}
+
+	if response.ContentBody != nil && *response.ContentBody != "This should be the body" {
+		t.Errorf("Unexpected body content: %s", response.ContentBody)
+	}
+
+}
+
+// Test Helperrs
+
+func stdLibHeadersToModel(header http.Header) []model.SingleItemMap {
+	var m []model.SingleItemMap
+	for key, values := range header {
+		for _, value := range values {
+			m = append(m, model.SingleItemMap{
+				Key:   util.StringPtr(key),
+				Value: util.StringPtr(value),
+			})
+		}
+	}
+	return m
+}
+
+func hasHeader(headers []model.SingleItemMap, findKey, findValue string) bool {
+	return util.Any(headers, func(key, value *string) bool {
+		return *key == findKey && *value == findValue
+	})
+}
+
+func printSingleItemMaps(headers []model.SingleItemMap) string {
+	reorganized := map[string]string{}
+
+	for _, h := range headers {
+		reorganized[*h.Key] = *h.Value
+	}
+	str, _ := json.Marshal(reorganized)
+	return string(str)
+}

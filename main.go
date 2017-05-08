adfec91e74b08cb34c360f2ac77553e4816f69e1
Jack Danger Canty <github@jackcanty.com>
2017-05-07 15:35:45 -1000

    command-specific flags

--- main.go
+++ main.go
@@ -18,15 +18,47 @@ import (
 	"github.com/JackDanger/traffic/transforms"
 )
 
-var port = flag.String("port", "8000", "Run server on 0.0.0.0 at this port")
+var serverFlags = flag.NewFlagSet("server", flag.ExitOnError)
+var workerFlags = flag.NewFlagSet("worker", flag.ExitOnError)
+var runnerFlags = flag.NewFlagSet("runner", flag.ExitOnError)
+
+// Server flags
+var port = serverFlags.String("port", "8000", "Run server on <hostname> at this port")
+var hostname = serverFlags.String("hostname", "0.0.0.0", "Bind to a network interface (if you want this to be accessible outside of a Docker container this must be '0.0.0.0')")
+
+// Worker flags
+var queue = workerFlags.String("queue", "localhost:7000", "The address of the queue that this worker should read from (not yet implemented)")
+
+// One-off HAR file runner flags
+var fileFlag = runnerFlags.String("harfile", "", "a .har file to replay")
+var archiveIDFlag = runnerFlags.String("archiveID", "", "the id of the archive record to replay")
+var velocityFlag = runnerFlags.String("velocity", "", "how fast to replay the archive (defaults to 1.0)")
+var concurrencyFlag = runnerFlags.String("concurrency", "", "how many threads to run in parallel")
 
 func main() {
-	flag.Parse()
-	runOneHar()
-	//runTheWebInterface()
+	// If there's just one argument then assume we need to print usage
+	if len(os.Args) == 1 {
+		fmt.Println("usage: traffic [server|runner] [args]")
+		return
+	}
+
+	switch os.Args[1] {
+	case "server":
+		serverFlags.Parse(os.Args[2:])
+		runServer()
+	case "worker":
+		workerFlags.Parse(os.Args[2:])
+	case "runner":
+		runnerFlags.Parse(os.Args[2:])
+		fmt.Printf("main.go:53 %#v\n", runnerFlags)
+		runOneHar()
+	default:
+		fmt.Printf("%q is not valid command.\n", os.Args[1])
+		os.Exit(2)
+	}
 }
 
-func runTheWebInterface() {
+func runServer() {
 	_, err := persistence.NewDb()
 	if err != nil {
 		fmt.Println(err.Error())
@@ -38,8 +70,8 @@ func runTheWebInterface() {
 		fmt.Println(err.Error())
 		return
 	}
-	web.Addr = "0.0.0.0"
-	fmt.Printf("Starting Traffic server on https://%s:%s\n", "0.0.0.0", *port)
+	web.Addr = fmt.Sprintf("%s:%s", *hostname, *port)
+	fmt.Printf("Starting Traffic server on https://%s\n", web.Addr)
 	err = web.ListenAndServeTLS("server/cert.pem", "server/key.pem")
 	if err != nil {
 		fmt.Printf("Error starting server: %#v", err.Error())
@@ -47,28 +79,22 @@ func runTheWebInterface() {
 }
 
 func runOneHar() {
-	var file = flag.String("harfile", "", "a .har file to replay")
-	var archiveID = flag.String("archiveID", "", "the id of the archive record to replay")
-	var velocityFlag = flag.String("velocity", "", "how fast to replay the archive (defaults to 1.0)")
-	var concurrencyFlag = flag.String("concurrency", "", "how many threads to run in parallel")
 
-	flag.Parse()
-
-	if *file == "" && *archiveID == "" {
+	if *fileFlag == "" && *archiveIDFlag == "" {
 		fmt.Printf("Specify a .har file to replay\n")
-		flag.PrintDefaults()
-		return
+		runnerFlags.PrintDefaults()
+		os.Exit(1)
 	}
 
 	var err error
 	var har *model.Har
-	if *file != "" {
-		har, err = parser.HarFromFile(*file)
+	if *fileFlag != "" {
+		har, err = parser.HarFromFile(*fileFlag)
 		fatalize(err)
 	} else {
 		db, err := persistence.NewDb()
 		fatalize(err)
-		id, err := strconv.Atoi(*archiveID)
+		id, err := strconv.Atoi(*archiveIDFlag)
 		fatalize(err)
 		archive, err := db.GetArchive(id)
 		fatalize(err)
@@ -104,7 +130,7 @@ func runOneHar() {
 	for i := 0; i < concurrency; i++ {
 		num := string('0' + i)
 		go func() {
-			name := filepath.Base(*file) + " #" + num
+			name := filepath.Base(*fileFlag) + " #" + num
 			runner := runner.NewHarRunner(har, runner.NewHTTPExecutor(name, os.Stdout), transforms, velocity)
 			runner.Run()
 			<-runner.GetDoneChannel()
@@ -126,7 +152,7 @@ func (h *handler) ServeHTTP(w http.ResponseWriter, request *http.Request) {
 // This starts a server and immediately backgrounds it via a goroutine
 func startLocalhostServerOnPort(port string) {
 	server := http.Server{
-		Addr:    "127.0.0.1:" + port,
+		Addr:    "0.0.0.0:" + port,
 		Handler: &handler{},
 	}
 	go server.ListenAndServe()
